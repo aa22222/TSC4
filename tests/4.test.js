a90bd61a1e14fc393ecaa39feb7463e7bc0c36cc
lukeboo28 <lukeboo28@yahoo.com>
2023-08-09 01:54:40 -0500

    Fix 4 and 5

--- tests/4.test.js
+++ tests/4.test.js
@@ -10,13 +10,43 @@ function simpleMessageCell(msg){
 function readSimpleMessageCell(cell){
     return cell.asSlice().skip(32).loadStringTail();
 }
-function caeserCipher(msg, i){
-    let re = "";
-    for(const c of msg){
-        re += String.fromCharCode(65 + (c.charCodeAt(0) - 65 + i + 26) % 26);
+function caeserCipher(str, amount) {
+    // Wrap the amount
+    if (amount < 0) {
+      return caesarShift(str, amount + 26);
     }
-    return re;
-}
+  
+    // Make an output variable
+    var output = "";
+  
+    // Go through each character
+    for (var i = 0; i < str.length; i++) {
+      // Get the character we'll be appending
+      var c = str[i];
+  
+      // If it's a letter...
+      if (c.match(/[a-z]/i)) {
+        // Get its code
+        var code = str.charCodeAt(i);
+  
+        // Uppercase letters
+        if (code >= 65 && code <= 90) {
+          c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+        }
+  
+        // Lowercase letters
+        else if (code >= 97 && code <= 122) {
+          c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+        }
+      }
+  
+      // Append
+      output += c;
+    }
+  
+    // All done!
+    return output;
+};
 const task = 4;
 async function main() {
     const code = fs.readFileSync(`../build/${task}.compiled.txt`).toString();
@@ -25,14 +55,19 @@ async function main() {
         new Cell(),
         { debug: true }
     )
-    const msg = "TESTMSG";
+    const msg = "TewruhwehfiuNHIOUEGFioueoIUWHROIEUFboiauhdseFUIEAEHOfiUHFoieUaushd";
     const cell = simpleMessageCell(msg);
 
     for(let i = 0; i < 26; i++){
         let re = await contract.invokeGetMethod("caesar_cipher_encrypt", [stackInt(i), stackCell(cell)]);
+        console.log(readSimpleMessageCell(re.result[0]))
+        console.log(caeserCipher(msg, i))
         assert(readSimpleMessageCell(re.result[0]) == caeserCipher(msg, i));
         let re2 = await contract.invokeGetMethod("caesar_cipher_decrypt", [stackInt(i), stackCell(re.result[0])]);
+        console.log(readSimpleMessageCell(re2.result[0]))
+        console.log(msg)
         assert(readSimpleMessageCell(re2.result[0]) == msg);
+        console.log(i);
     }
     // console.log(re.logs);
     // printLogs(re.debugLogs);
--- tests/5.test.js
+++ tests/5.test.js
@@ -18,15 +18,14 @@ async function main() {
     for(let i = 0; i < 400; i++){
         fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
     }
-    
-    for(let n = 0; n <= 370; n++ ){
+    for(let n = 1; n <= 370; n++ ){
         for(let k = 0; k <= Math.min(370 - n, 255); k++){
             let re = await contract.invokeGetMethod('fibonacci_sequence', [stackInt(n), stackInt(k)]);
-            assert(re.result[0].length == k+1);
-            for(let i = 0; i <= k; i++){
+            assert(re.result[0].length == k);
+            for(let i = 0; i < k; i++){
                 assert(re.result[0][i] == fib[n+i]);
             }
-            if(k % 10 == 0) console.log((n, k));
+            if(k % 15 == 0) console.log(`${n} ${k}`);
         }
     }
     
